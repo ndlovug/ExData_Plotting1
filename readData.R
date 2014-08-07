84a7719003fe41bd1e94462e2be7ba3e13b7e55a
ndlovug <ndlovug@gmail.com>
2014-08-07 12:25:39 -0700

    This is the R script that reads in the data and it is called by the four scripts that produce the plots.

---
+++ readData.R
@@ -0,0 +1,26 @@
+# clear list
+rm(list = ls())
+
+# load library
+library(sqldf)
+
+# path and file string
+hpcfile <- "exdata-data-household_power_consumption/household_power_consumption.txt"
+
+## read file and select rows based on string "1/1/2007" and store in dataframe DF
+hpcraw <- read.csv.sql(hpcfile, sep=";",sql = 'Select * From file where Date = "1/2/2007" OR Date = "2/2/2007" ')
+
+# close connection
+close(hpcfile)
+
+# check to see if there's missing data
+# which(apply(X = hpcraw, MARGIN = 2, FUN = function(x) any(grepl("\\?", x))))
+
+# concatenate Date & Time columns to create a datetime column
+hpcraw$datetime <- with(hpcraw, paste(Date, Time, sep = " "))
+
+# format datetime as date
+hpcraw$datetime <- with(hpcraw, strptime(x = datetime, format = "%d/%m/%Y %H:%M:%S"))
+
+
+
