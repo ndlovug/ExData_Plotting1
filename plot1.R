ff9d190b73d29f623dafc5ad7188cc450207feb5
ndlovug <ndlovug@gmail.com>
2014-08-07 23:28:23 -0700

    Edited the script to print directly to file device.

--- plot1.R
+++ plot1.R
@@ -3,22 +3,22 @@ source("readData.R")
 
 hpcdf <- hpcraw
 
+# set plot size 
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+
+# set no background
 par(bg = NA)
 
-# plot the histogram to screen device
+# plot the histogram to file device
 with(hpcdf, hist(x = Global_active_power, 
                  col = "red", #histogram fill color
                  main = paste("Global Active Power"), #main title
                  xlab = "Global Active Power (Kilowatts)", #x-axis title
-                 cex.main = 0.9, #reduce magnification of main title
-                 cex.lab = 0.8, #reduce magnification of x & y labels
-                 cex.axis = 0.75 # reduce magnification for axis annotation
+                 #cex.main = 0.9, #reduce magnification of main title
+                 #cex.lab = 0.8, #reduce magnification of x & y labels
+                 #cex.axis = 0.75 # reduce magnification for axis annotation
                  )
      )
-
-
-# copy from screen device to png file device
-dev.copy(png, file = "plot1.png")
 
 # close the file device
 dev.off()
